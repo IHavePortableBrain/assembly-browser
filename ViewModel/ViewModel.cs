184dad56e4a03c7efe730a756e349feb74b4c070
IHavePortableBrain <vlad.krs7@gmail.com>
2019-10-11 09:06:59 +0300

    MVVM skeleton added.

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class ViewModelMediator : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private int _number1;
+        public int Number1
+        {
+            get { return _number1; }
+            set
+            {
+                _number1 = value;
+                OnPropertyChanged("Number3"); // уведомление View о том, что изменилась сумма
+            }
+        }
+
+        private int _number2;
+        public int Number2
+        {
+            get { return _number2; }
+            set { _number2 = value; OnPropertyChanged("Number3"); }
+        }
+        public int Number3 => MathFuncs.GetSumOf(Number1, Number2);
+    }
+}
